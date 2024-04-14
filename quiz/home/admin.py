21a6e172ca163f13487576f02bf9616cfe612ca8
Lukesha Patil <166599771+lukeshap@users.noreply.github.com>
2024-04-14 13:40:26 +0530

    Add files via upload

---
+++ quiz/home/admin.py
@@ -0,0 +1,17 @@
+# from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+class AnswerAdmin(admin.StackedInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+admin.site.register(Types)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
